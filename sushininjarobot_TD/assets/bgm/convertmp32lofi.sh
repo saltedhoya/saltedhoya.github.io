dcc9963425fe24b53e39ad527b160d75640feb51
Koji Horaguchi <koji.horaguchi@gmail.com>
2025-05-03 17:33:30 +0900

    Update games

--- sushininjarobot_TD/assets/bgm/convertmp32lofi.sh
+++ sushininjarobot_TD/assets/bgm/convertmp32lofi.sh
@@ -1,5 +1,6 @@
 for f in *.mp3
 do
     mv ${f} high_${f}
-    ffmpeg -i high_${f} -vn -ac 1 -ab 64k ${f}
+    echo ffmpeg -loglevel error -i high_${f} -vn -ac 1 -ab 64k ${f}
+    ffmpeg -loglevel error -i high_${f} -vn -ac 1 -ab 64k ${f}
 done
